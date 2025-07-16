f088a09a6fd2954be5fee49e7de13ecfabeff5d9
Sirraide <aeternalmail@gmail.com>
2025-07-17 00:15:17 +0200

    [IR] Add finalisation pass

---
+++ src/CG/IR/IRLowering.cc
@@ -0,0 +1,24 @@
+#include <srcc/CG/CodeGen.hh>
+#include <srcc/CG/IR/IR.hh>
+
+#include <mlir/Pass/PassManager.h>
+#include <mlir/Transforms/Passes.h>
+
+using namespace srcc;
+using namespace srcc::cg;
+
+auto CodeGen::emit_llvm(llvm::TargetMachine&) -> std::unique_ptr<llvm::Module> {
+    Todo();
+}
+
+bool CodeGen::finalise() {
+    mlir::PassManager pm{&mlir};
+    pm.enableVerifier(false);
+    pm.addPass(mlir::createCanonicalizerPass());
+    pm.addPass(mlir::createRemoveDeadValuesPass());
+    if (pm.run(mlir_module).failed()) {
+        ICE(Location(), "Failed to finalise IR");
+        return false;
+    }
+    return true;
+}
